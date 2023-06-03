de52583f7d57b89889d643d2c08f152a15e7feb9
josiel <josiel.jcc@hotmail.com>
2023-06-03 17:46:14 -0300

    feat: create branch release

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema } from "@/validations/loginSchema";
 import { z } from "zod";
+//
 
 type LoginFormData = z.infer<typeof loginSchema>;
 
