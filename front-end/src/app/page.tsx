0de9d064d62a32ec1dca482056429771a5fd2fb4
josiel <josiel.jcc@hotmail.com>
2023-06-03 15:11:10 -0300

    feat: learn how create routes in next.js

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-800 p-24">
-      
     </main>
   )
 }
