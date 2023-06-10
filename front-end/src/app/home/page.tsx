14c515455e14c464df3f92a8538028e2833172d4
josiel <josiel.jcc@hotmail.com>
2023-06-10 14:03:13 -0300

    feat: buttons next page and prev page in home

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { getApiRecipes } from "../utils/apiFunctions";
 import { IRecipe } from "../interfaces/IRecipe";
 import axios from "axios";
 import RecipeCard from "../components/RecipeCard";
@@ -8,11 +7,21 @@ async function Home() {
   const response = await axios.get("http://192.168.1.107:3001/recipes");
   const { recipes } = response.data;
   return (
-    <div className="min-h-screen flex bg-zinc-800 text-zinc-300">
+    <div className="min-h-screen px-4 flex bg-zinc-800 text-zinc-300">
+      <div className="sticky h-screen flex  items-center top-0">
+        <button className="rotate-180 flex items-center justify-center rounded-full h-12 p-4 bg-zinc-700 text-zinc-300">
+          ➤
+        </button>
+      </div>
       <div className="w-[90%] m-auto my-8 justify-center flex flex-wrap gap-4">
         {recipes.map((recipe: IRecipe) => (
           <RecipeCard {...recipe} key={recipe.ID} />
         ))}
+      </div>
+      <div className="sticky h-screen flex  items-center top-0">
+        <button className="flex items-center justify-center rounded-full h-12 p-4 bg-zinc-700 text-zinc-300">
+          ➤
+        </button>
       </div>
     </div>
   );
