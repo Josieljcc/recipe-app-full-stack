ce1f64fcf69c430e58ed2ef5a30e33a08785dae3
josieljcc <josiel.jcc@hotmail.com>
2023-06-16 22:16:37 -0300

    chore: remove commentary

--- back-end/src/database/database.go
+++ back-end/src/database/database.go
@@ -9,7 +9,6 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:senhaDoDB@tcp(db:3306)/app-recipes-db?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
