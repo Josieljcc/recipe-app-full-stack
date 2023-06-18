2e0dba2cd8e0a24025f94522677bb0ae2cb415f7
josieljcc <josiel.jcc@hotmail.com>
2023-06-18 18:09:29 -0300

    feat: add favorite button in details

--- front-end/src/app/recipe/[id]/page.tsx
+++ front-end/src/app/recipe/[id]/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import { IUser } from "@/app/interfaces";
 import { IRecipe } from "@/app/interfaces/IRecipe";
+import {
+  getFavorites,
+  getRecipeById,
+  postFavorite,
+} from "@/app/utils/apiFunctions";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
 type params = {
   params: {
@@ -7,18 +18,71 @@ type params = {
   };
 };
 
-async function Detail({ params: { id } }: params) {
-  const response = await axios(`http://192.168.1.107:3001/recipe/${id}`);
-  const recipe: IRecipe = response.data.recipe;
+function Detail({ params: { id } }: params) {
+  const [recipe, setRecipe] = useState<IRecipe>();
+  const [favorites, setFavorites] = useState<IRecipe[]>([]);
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    async function fetchRecipe() {
+      const user = localStorage.getItem("user");
+      if (!user) return;
+      const { token } = JSON.parse(user);
+      setToken(token);
+      const recipe = await getRecipeById(id);
+      const favorites = await getFavorites(token);
+      setFavorites(favorites);
+      setRecipe(recipe);
+    }
+    fetchRecipe();
+  }, [id]);
+
+  function isFavorite() {
+    return favorites.some((favorite) => favorite.ID === recipe?.ID);
+  }
+
+  async function handleFavorite() {
+    if (!isFavorite() && recipe) {
+      await postFavorite(token, recipe.ID);
+      const favorites = await getFavorites(token);
+      setFavorites(favorites);
+    }
+  }
+
+  const router = useRouter();
   const BASE_IMAGE_URL = "http://localhost:3001/images/";
+
+  if (!recipe) return null;
+
   return (
     <div className="bg-zinc-800 text-zinc-300 min-h-screen">
       <div className="relative">
-        <div className="absolute bg-gradient-to-b rounded-2xl to-black/70 from-transparent z-10 w-full h-full" />
+        <div
+          className="absolute bg-gradient-to-b rounded-2xl to-black/80 
+        from-transparent z-10 w-full h-full"
+        />
+        <button
+          className="absolute p-2 top-4 left-4
+        text-2xl z-20 bg-zinc-700/80 rounded-full"
+          onClick={() => router.back()}
+        >
+          <BiArrowBack />
+        </button>
+        <button
+          className="absolute p-2 top-4 right-4
+         z-20 bg-zinc-700/80 rounded-full"
+          onClick={handleFavorite}
+        >
+          {isFavorite() ? (
+            <MdFavorite className="text-2xl text-red-600" />
+          ) : (
+            <MdFavoriteBorder className="text-2xl" />
+          )}
+        </button>
         <img
-          className="w-full rounded-b-2xl"
+          className="w-full h-full rounded-b-2xl"
           src={`${BASE_IMAGE_URL}${recipe.image}.jpg`}
-          alt={recipe.image}
+          alt={recipe.title}
         />
         <h1
           className="absolute bottom-4 w-full left-0 right-0 m-auto 
--- front-end/src/app/utils/apiFunctions.ts
+++ front-end/src/app/utils/apiFunctions.ts
@@ -16,7 +16,25 @@ export async function getRecipes (page: string): Promise<IRecipe[]> {
     return response.data.recipes
 }
 
+export async function getRecipeById (id: string): Promise<IRecipe> {
+    const response = await axios.get(`http://localhost:3001/recipe/${id}`)
+    return response.data.recipe
+}
+
 export async function getRecipeBySearch (search: string): Promise<IRecipe[]> {
     const response = await axios.get(`http://localhost:3001/recipes/search/?q=${search}`)
     return response.data.recipes
+}
+
+export async function getFavorites (token: string): Promise<IRecipe[]> {
+    const response = await axios.get(`http://localhost:3001/favorite`,
+    { headers: { Authorization: token } })
+    return response.data.favorites
+}
+
+export async function postFavorite (token: string, id: number): Promise<void> {
+    await axios.post(`http://localhost:3001/favorite`,
+        { "recipeId": id  },
+        { headers: { Authorization: token } },
+    )
 }
