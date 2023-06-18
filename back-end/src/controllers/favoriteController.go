3a74ee5738a7d8e3452dd7ebdc3f48affd669e2e
josieljcc <josiel.jcc@hotmail.com>
2023-06-18 18:55:59 -0300

    feat: delete favorite from user

--- back-end/src/controllers/favoriteController.go
+++ back-end/src/controllers/favoriteController.go
@@ -35,3 +35,22 @@ func InsertFavoriteRecipe(c *gin.Context) {
 		"message": body,
 	})
 }
+
+func RemoveFavoriteRecipe(c *gin.Context) {
+	var body struct {
+		RecipeId uint `json:"recipeId"`
+	}
+	c.Bind(&body)
+	id := c.MustGet("id").(float64)
+	result := services.RemoveFavoriteRecipe(id, body.RecipeId)
+	if result != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":   result,
+			"message": "error to remove favorite",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": body,
+	})
+}
