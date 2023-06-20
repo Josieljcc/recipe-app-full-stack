ca977103f437a08e984bf2866e4f8635558090de
josieljcc <josiel.jcc@hotmail.com>
2023-06-20 17:22:15 -0300

    feat: add income validations to user and login

--- back-end/src/interfaces/userInterface.go
+++ back-end/src/interfaces/userInterface.go
@@ -1,6 +1,7 @@
 package interfaces
 
-type ILogin struct {
+type IUser struct {
 	Password string `json:"password" binding:"required,min=6"`
 	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
 }
