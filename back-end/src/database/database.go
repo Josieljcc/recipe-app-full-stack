20595af35312cf57cfded7743dfd41a07b3e5e33
josieljcc <josiel.jcc@hotmail.com>
2023-06-20 17:21:38 -0300

    refactor: database.go error handle

--- back-end/src/database/database.go
+++ back-end/src/database/database.go
@@ -7,11 +7,12 @@ import (
 
 var DB *gorm.DB
 
-func ConnectToDB() {
+func ConnectToDB() error {
 	var err error
 	dsn := "root:senhaDoDB@tcp(db:3306)/app-recipes-db?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+	return nil
 }
