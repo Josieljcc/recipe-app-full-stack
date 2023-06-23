73c30f1d82e5e31ea84a7bd4b1d4069e591ef241
josieljcc <josiel.jcc@hotmail.com>
2023-06-23 16:34:00 -0300

    refactor: import of doodle.svg

--- front-end/src/app/components/Header/Header.tsx
+++ front-end/src/app/components/Header/Header.tsx
@@ -3,7 +3,6 @@ import Input from "../Input";
 import { useForm } from "react-hook-form";
 import Image from "next/image";
 import { AiOutlineSearch } from "react-icons/ai";
-import doodle from "@/../public/doodle.svg";
 import { searchSchema } from "@/app/validations/searchSchema";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,9 +22,9 @@ function Header({ search }: { search: (data: searchFormData) => void }) {
     <div className="fixed top-0 left-0 h-36 w-full z-30 rounded-b-3xl bg-black shadow-lg shadow-zinc-950/60 flex justify-center items-center">
       <div className="absolute w-full h-full top-0 rounded-b-3xl bg-black/50" />
       <div className="absolute w-full h-full top-0 rounded-b-3xl bg-gradient-to-t from-red-600/30 to-transparent" />
-      <Image
+      <img
         className="absolute top-0 w-full h-full object-cover rounded-b-3xl"
-        src={doodle}
+        src={"/doodle.svg"}
         alt="doodle"
       />
       <form onSubmit={handleSubmit(search)} className="z-30 absolute">
