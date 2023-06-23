6c587d97bea06b1f1389957cf99c411817c2bf64
josieljcc <josiel.jcc@hotmail.com>
2023-06-23 16:21:13 -0300

    refactor: password lenght

--- front-end/src/app/validations/loginSchema.ts
+++ front-end/src/app/validations/loginSchema.ts
@@ -2,5 +2,5 @@ import z from 'zod'
 
 export const loginSchema = z.object({
     email: z.string().email().nonempty().toLowerCase(),
-    password: z.string().min(6, 'The password must have at least 6 characters').nonempty(),
+    password: z.string().min(8, 'The password must have at least 8 characters').nonempty(),
 })
